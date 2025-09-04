5c7d57fb67b8bc3c2e3d37ba24230f9cf3e67709
whtlconsult-ops <whtl.consult@gmail.com>
2025-09-04 11:50:44 +0200

    Fix 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
+
+// Forza il rendering client-side ed evita prerender/SSG
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import App from "./widget/App";
 
 export default function Page() {
   return <App />;
 }
-
