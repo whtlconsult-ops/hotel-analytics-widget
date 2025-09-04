e2babc3340c70852faa4c2e6328c3a4036f9e181
whtlconsult-ops <whtl.consult@gmail.com>
2025-09-04 12:15:29 +0200

    Fix 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-export const dynamic = "force-dynamic"; // evita prerender lato server
-export const revalidate = 0;             // nessuna cache/SSG
+export const dynamic = "force-dynamic"; // disattiva il prerender/SSG di questa pagina
 export const fetchCache = "force-no-store";
 
 import App from "./widget/App";
